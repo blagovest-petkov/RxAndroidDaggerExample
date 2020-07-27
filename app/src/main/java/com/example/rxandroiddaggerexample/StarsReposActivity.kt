350a832efda0607fd3c8985bcfee8d2a07d14426
Blagovest Petkov <blagovest.petkov@NSOGROUP.COM>
2020-07-27 17:48:24 +0300

    Add api client, fix bugs

--- app/src/main/java/com/example/rxandroiddaggerexample/StarsReposActivity.kt
+++ app/src/main/java/com/example/rxandroiddaggerexample/StarsReposActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rxandroiddaggerexample.adapter.GithubRepoAdapter
 import com.example.rxandroiddaggerexample.model.Repo
+import com.example.rxandroiddaggerexample.network.GithubApiClient
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_stars_repos.*
 
 class StarsReposActivity : AppCompatActivity() {
     //----------------------------------------------------------------------------------------------
     // Fields
     //----------------------------------------------------------------------------------------------
-    private val repoList:ArrayList<Repo> = ArrayList()
+    private lateinit var adapter:GithubRepoAdapter;
 
     //----------------------------------------------------------------------------------------------
     // Lifecycle
@@ -19,20 +22,20 @@ class StarsReposActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stars_repos)
 
-        val adapter = GithubRepoAdapter()
-
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-        repoList.add(Repo("RxJava", "Rx Java for android", "Java"))
-
-        adapter.addData(repoList)
+        adapter = GithubRepoAdapter()
         rvStarsRepo.adapter = adapter
+
+        getStarredRepos()
     }
+
+    fun getStarredRepos() {
+        GithubApiClient.githubService.getStarredRepos("mrabelwahed")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                it -> adapter.addData(it)
+            }
+
+    }
+
 }
